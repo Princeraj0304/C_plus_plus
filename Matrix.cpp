70746ae5c13095fa32255073e279a31aa5b8a05d
Prince Raj <priinceraj5403@gmail.com>
2024-12-07 23:51:40 +0530

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+    for (int i = 0; i < 2; i++)
+    {
+        for(int j=0;j<2;j++){
+            cout<<i<<j<<" ";
+        }
+        cout<<endl;
+    }
+    
+
+return 0;
+}
